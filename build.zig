04195b5efd2aa51eecbeb60dddcbf8a00a6dc456
Sungbae Jeong <almagest0328@gmail.com>
2023-12-20 05:50:09 +0900

    start the project!

---
+++ build.zig
@@ -0,0 +1,104 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const version = try std.SemanticVersion.parse("0.1.0");
+
+    const exe = b.addExecutable(.{
+        .name = "pixeka",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+        .version = version,
+    });
+    linkRaylib(b, exe, target, optimize);
+    exe.linkLibC();
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/root.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+// NOTE: Stolen from https://github.com/Not-Nik/raylib-zig/blob/devel/build.zig
+fn linkRaylib(
+    b: *std.Build,
+    exe: *std.Build.Step.Compile,
+    target: std.zig.CrossTarget,
+    optimize: std.builtin.Mode,
+) void {
+    const raylib = b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const art = raylib.artifact("raylib");
+
+    const target_os = exe.target.toTarget().os.tag;
+    switch (target_os) {
+        .windows => {
+            exe.linkSystemLibrary("winmm");
+            exe.linkSystemLibrary("gdi32");
+            exe.linkSystemLibrary("opengl32");
+        },
+        .macos => {
+            exe.linkFramework("OpenGL");
+            exe.linkFramework("Cocoa");
+            exe.linkFramework("IOKit");
+            exe.linkFramework("CoreAudio");
+            exe.linkFramework("CoreVideo");
+        },
+        .freebsd, .openbsd, .netbsd, .dragonfly => {
+            exe.linkSystemLibrary("GL");
+            exe.linkSystemLibrary("rt");
+            exe.linkSystemLibrary("dl");
+            exe.linkSystemLibrary("m");
+            exe.linkSystemLibrary("X11");
+            exe.linkSystemLibrary("Xrandr");
+            exe.linkSystemLibrary("Xinerama");
+            exe.linkSystemLibrary("Xi");
+            exe.linkSystemLibrary("Xxf86vm");
+            exe.linkSystemLibrary("Xcursor");
+        },
+        .emscripten, .wasi => {
+            // When using emscripten, the libries don't need to be linked
+            // because emscripten is going to do that later.
+        },
+        else => { // Linux and possibly others.
+            exe.linkSystemLibrary("GL");
+            exe.linkSystemLibrary("rt");
+            exe.linkSystemLibrary("dl");
+            exe.linkSystemLibrary("m");
+            exe.linkSystemLibrary("X11");
+        },
+    }
+
+    exe.linkLibrary(art);
+}
---
+++ src/main.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const rl = @import("raylib.zig");
+
+const SCREEN_WIDTH = 800;
+const SCREEN_HEIGHT = 600;
+
+pub fn main() void {
+    rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "foo");
+    defer rl.CloseWindow();
+
+    rl.SetTargetFPS(60);
+
+    while (!rl.WindowShouldClose()) {
+        {
+            rl.BeginDrawing();
+            defer rl.EndDrawing();
+
+            rl.ClearBackground(rl.RAYWHITE);
+            rl.DrawText("This is a first window using zig", 190, 200, 20, rl.LIGHTGRAY);
+        }
+    }
+}
